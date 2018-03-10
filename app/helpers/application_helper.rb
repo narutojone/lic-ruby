1f8d6eba5f548c3d005782ce748324fa3558c439
SamQuiDaiBo <SamTranNgocQB@yahoo.com>
2018-03-10 12:09:03 +0700

    Display error message when fail to login.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,15 @@ module ApplicationHelper
   def active_when(path)
     request.fullpath == path ? "active" : nil
   end
+
+  def alert_type_of(message_type)
+    case message_type.to_s
+    when "alert"
+      "danger"
+    when "notice"
+      "info"
+    else
+      message_type
+    end
+  end
 end
