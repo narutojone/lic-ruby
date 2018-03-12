340df1a23e66fb394beb9b8782eb682aacd91377
SamQuiDaiBo <SamTranNgocQB@yahoo.com>
2018-03-12 09:25:43 +0700

    Make edit all roles works.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -5,3 +5,11 @@
 //
 
 jQuery("input, textarea, select").blur();
+jQuery(document).on("click", ".js-back-link", function(event) {
+  var href = jQuery(this).attr("href");
+
+  if (href == "#") {
+    event.preventDefault();
+    history.back();
+  }
+});
